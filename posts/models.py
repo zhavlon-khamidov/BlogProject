81ff98d0b9ccc313d364c6073f5d6a2602b05457
Mr.Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2023-04-12 16:33:53 +0600

    Post & Tag models added

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    # post_image
+    # author
+    tags = models.ManyToManyField(Tag, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    
+    def __str__(self):
+        return self.title
+    
